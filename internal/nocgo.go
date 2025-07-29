74c52548d7d6be680d05a4a31058979bf6aef092
Antoni Szymański <antoni.szymanski+github@proton.me>
2025-07-29 23:20:31 +0200

    chore: update license headers to SPDX format

--- internal/nocgo.go
+++ internal/nocgo.go
@@ -1,10 +1,7 @@
-//go:build !cgo
+// SPDX-FileCopyrightText: 2025 Antoni Szymański
+// SPDX-License-Identifier: MPL-2.0
 
-/*
-This Source Code Form is subject to the terms of the Mozilla Public
-License, v. 2.0. If a copy of the MPL was not distributed with this
-file, You can obtain one at https://mozilla.org/MPL/2.0/.
-*/
+//go:build !cgo
 
 package internal
 
--- internal/nocgo/load_unix.go
+++ internal/nocgo/load_unix.go
@@ -1,10 +1,7 @@
-//go:build darwin || freebsd || linux || netbsd
+// SPDX-FileCopyrightText: 2025 Antoni Szymański
+// SPDX-License-Identifier: MPL-2.0
 
-/*
-This Source Code Form is subject to the terms of the Mozilla Public
-License, v. 2.0. If a copy of the MPL was not distributed with this
-file, You can obtain one at https://mozilla.org/MPL/2.0/.
-*/
+//go:build darwin || freebsd || linux || netbsd
 
 package nocgo
 
